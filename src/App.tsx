6d4765a2e236a5436250c4055136d7d71e16682e
Gabriel Almeida <biel88sud@gmail.com>
2025-05-15 07:48:36 -0300

    components for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-//import styles from './styles/global.css'
+import './styles/theme.css'
+import './styles/global.css'
+
+import { Container } from './components/Container'
+import { Heading } from './components/Heading'
 
 export function App() {
     return (
-        <>
-            <h1>Chronos</h1>
-        </>
+      <>
+        <Container>
+            <Heading>
+              Logo
+            </Heading>
+        </Container>
+        <Container>
+          <Heading>
+              Menu
+            </Heading>
+        </Container>
+        <Container>
+            <Heading>
+              Timer
+            </Heading>
+        </Container>
+        <Container>
+            <Heading>
+              Descrição
+            </Heading>
+        </Container>
+        <Container>
+          <Heading>
+              Footer
+            </Heading>
+        </Container>
+      <Container>
+          <div className='content'>
+            <Heading>
+              Footer
+            </Heading>
+          </div>
+        </Container>
+      </>
+      
     )
 }
