8f8654348100c8c875a26504e432a8db13817e12
Gabriel Almeida <biel88sud@gmail.com>
2025-05-15 08:08:20 -0300

    added Logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,12 @@
+import { TimerIcon } from 'lucide-react';
 import styles from './styles.module.css';
 
-type LogoProps = {
-    children: React.ReactNode;
-}
-
-export function Logo({ children }: LogoProps) {
-    return <h1 className={styles.logo}>{children}</h1>;
+export function Logo() {
+    return <div className={styles.logo}>
+        <a className={styles.logoLink} href="">
+            <TimerIcon />
+            <span>Chronos</span>
+        </a>
+    </div>;
 }
     
