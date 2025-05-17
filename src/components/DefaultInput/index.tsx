4192171a12493ac3a97867ec7aa89e49ac488616
Gabriel Almeida <biel88sud@gmail.com>
2025-05-17 13:20:32 -0300

    styling component menu

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,13 +1,16 @@
+import styles from './styles.module.css'
+
 type DefaultInputProps = {
     id: string;
     labelText: string;
+    placeholder?: string;
 } & React.ComponentProps<'input'>
 
-export function DefaultInput({id, labelText }: DefaultInputProps) {
+export function DefaultInput({id, labelText, placeholder }: DefaultInputProps) {
     return (
         <>
             <label htmlFor={id}>{labelText}</label>
-            <input id={id} />
+            <input id={id} className={styles.input} placeholder={placeholder} />
         </>
     )
 }
